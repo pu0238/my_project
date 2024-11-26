223f3a2c092219072d7e0d4bdd68c34c59b3ae4d
pu0238 <pu0238@protonmail.com>
2024-11-26 12:29:45 +0000

    Created firs version of chat

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,14 +1,25 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let chat = ref([]);
+
+async function getChats() {
+  chat.value = await my_project_backend.get_chat()
+}
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
   const chat = target.querySelector('#chat').value;
   await my_project_backend.save_chat(chat)
+  await getChats()
 }
+
+async function login() {
+
+}
+
+getChats()
 </script>
 
 <template>
@@ -21,6 +32,8 @@ async function handleSubmit(e) {
       <input id="chat" alt="Name" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="chat">
+      <div v-for="msg in chat">{{ msg }}</div>
+    </section>
   </main>
 </template>
